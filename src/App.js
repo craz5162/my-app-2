2a51965455a2d4489f4048e8d111d0eef70b0240
arfathusain <arfathusain5162@gmail.com>
2022-12-11 17:16:30 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
+  const [Countries,setCountries] = useState([]);
+  useEffect(()=>{
+    fetch('https://restcountries.com/v3.1/all')
+      .then(res => res.json())
+      .then(data => setCountries(data))
+  },[])
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>hi iam arfat who are you : {Countries.length}</h1>
+      
+      <div className="data-2">
+      {
+        Countries.map(country => <Country name ={country.name.common} area ={country.area}
+          img ={country.flags.png} population ={country.population}
+        ></Country>)
+      }
+      </div>
     </div>
   );
+}
+const Country = (Props) =>{
+  return(
+    <div className='data'>
+      <img src={Props.img} alt="" />
+      <h2>Name : {Props.name}</h2>
+      <h3>area : {Props.area}</h3>
+      <h4>population : {Props.population}</h4>
+    </div>
+  )
 }
 
 export default App;
